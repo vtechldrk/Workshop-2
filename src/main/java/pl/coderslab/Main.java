e25d9b50789818abfe073884454b3a0ee64bfcb1
Witek Wasilkowski <witek@wasilkowski.pl>
2023-02-08 15:14:00 +0100

    First Commit

--- src/main/java/pl/coderslab/Main.java
+++ src/main/java/pl/coderslab/Main.java
@@ -1,7 +1,35 @@
 package pl.coderslab;
 
+import pl.coderslab.entity.User;
+import pl.coderslab.entity.UserDao;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        User user = new User();
+        UserDao userDao = new UserDao();
+
+        user.setUserName("testUser7");
+        user.setEmail("test@email19.com");
+        user.setPassword("haslo7");
+  //      userDao.create(user);
+        System.out.println(user.getId());
+        User read = userDao.read(1);
+        System.out.println("ID: " + read.getId() + "\nUsername: " + read.getUserName() + "\nPassword: " +read.getPassword() + "\nemail: " + read.getEmail());
+        System.out.println(userDao.hashPassword("haslo7"));
+
+        User update = new User();
+        update.setUserName("testupdate");
+        update.setEmail("update@email31.com");
+        update.setPassword("StareHaslo");
+        userDao.create(update);
+        User read2 = userDao.read(update.getId());
+        System.out.println("ID: " + read2.getId() + "\nUsername: " + read2.getUserName() + "\nPassword: " +read2.getPassword() + "\nemail: " + read2.getEmail());
+
+        read2.setEmail("nowy@email.com");
+        read2.setPassword("NoweHaslo");
+
+        userDao.update(read2);
+
     }
 }
---
+++ src/main/java/pl/coderslab/entity/UserDao.java
@@ -0,0 +1,80 @@
+package pl.coderslab.entity;
+
+import org.mindrot.jbcrypt.BCrypt;
+import pl.coderslab.DbUtil;
+import java.sql.*;
+
+
+public class UserDao {
+    private static final String CREATE_USER_QUERY =
+            "INSERT INTO users(username, email, passwd) VALUES (?, ?, ?)";
+    private static final String READ_USER_QUERY =
+            "SELECT * FROM users WHERE id = ?";
+    private static final String UPDATE_USER_QUERY =
+            "UPDATE users SET username = ?, email = ?, passwd = ? WHERE id = ?";
+
+    public String hashPassword(String password) {
+        return BCrypt.hashpw(password, BCrypt.gensalt());
+    }
+
+    public User create(User user) {
+        try (Connection conn = DbUtil.getConnection()){
+            PreparedStatement statement =
+                    conn.prepareStatement(CREATE_USER_QUERY, Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1, user.getUserName());
+            statement.setString(2, user.getEmail());
+            statement.setString(3, hashPassword(user.getPassword()));
+            statement.executeUpdate();
+            //Pobieramy wstawiony do bazy identyfikator, a następnie ustawiamy id obiektu user.
+            ResultSet resultSet = statement.getGeneratedKeys();
+            if (resultSet.next()) {
+                user.setId(resultSet.getInt(1));
+            }
+            return user;
+        } catch (SQLException e) {
+            System.out.println("Błąd dodania rekordu." + e.getErrorCode());
+            //e.printStackTrace();
+            return null;
+        }
+    }
+    public User read(int userId) {
+
+        try (Connection conn = DbUtil.getConnection()){
+            PreparedStatement statement = conn.prepareStatement(READ_USER_QUERY);
+            statement.setInt(1, userId);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getInt("id"));
+                user.setUserName(resultSet.getString("username"));
+                user.setEmail(resultSet.getString("email"));
+                user.setPassword(resultSet.getString("passwd"));
+                return user;
+            } else {
+                System.out.println("Brak szukanego id w bazie danych");
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void update(User user) {
+        try (Connection conn = DbUtil.getConnection()){
+            PreparedStatement statement =
+                    conn.prepareStatement(UPDATE_USER_QUERY);
+            statement.setString(1, user.getUserName());
+            statement.setString(2, user.getEmail());
+            statement.setString(3, hashPassword(user.getPassword()));
+            statement.setInt(4, user.getId());
+
+            statement.executeUpdate();
+
+        } catch (SQLException e) {
+            System.out.println("Błąd modyfikacji rekordu." + e.getErrorCode());
+            //e.printStackTrace();
+        }
+    }
+
+}
